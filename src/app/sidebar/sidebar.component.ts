2991d3b4bbd985cb8b8ecbce1d917f8fe16eba9d
blacksavior88 <coreyhurst@hotmail.com>
2022-11-06 22:15:59 -0500

    Initial Commit

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent {
+
+  folders: string[] = ['My Story','Software Engineer','Mental Health Advocate','Health Enthusiast',
+		'Gamer','Artist','Entertainer','Avid Reader','Master Chef'];
+  projects: string[] = ['This Portfolio site','RRPG Bot','Cookbooks'];
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
