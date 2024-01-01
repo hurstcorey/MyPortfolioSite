f57a91c2b85f407a110ecc141aee1979714e8d34
Corey Lee Hurst <coreyhurst@hotmail.com>
2024-01-01 11:42:09 -0500

    Added Amplify backend. APIs and Database. Reorganized Components.

--- src/app/components/AboutMe/AboutSection.tsx
+++ src/app/components/AboutMe/AboutSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
-import TabButton from "./TabButton";
+import TabButton from "../TabButton";
 
 const TAB_DATA = [
   {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import React, { ReactNode } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,7 +9,7 @@ export const metadata = {
   description: 'Welcome to my portfolio',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
