74856c5ffc0742e01a2f19d3de4c93649a2c1bf2
Corey Lee Hurst <coreyhurst@hotmail.com>
2024-01-20 09:54:17 -0500

    Update dependencies and remove unused code

--- src/app/components/AboutMe/AchievementsSection.tsx
+++ src/app/components/AboutMe/AchievementsSection.tsx
@@ -1,32 +1,25 @@
 "use client";
 import React from "react";
-import dynamic from "next/dynamic";
-
-const AnimatedNumbers = dynamic(
-  () => {
-    return import("react-animated-numbers");
-  },
-  { ssr: false }
-);
+import AnimatedNumbers from "react-animated-numbers";
 
 const achievementsList = [
   {
     metric: "Projects",
     value: "0",
-    postfix: "+",
+    postfix: "10+",
   },
   {
     prefix: "~",
     metric: "Users",
-    value: "0",
+    value: "1000",
   },
   {
     metric: "Awards",
-    value: "0",
+    value: "5",
   },
   {
     metric: "Years",
-    value: "0",
+    value: "9",
   },
 ];
 
@@ -34,7 +27,7 @@ const AchievementsSection = () => {
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
       <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
-        {achievementsList.map((achievement, index) => {
+        {achievementsList.map((achievement) => {
           return (
             <div
               key={achievement.metric}
