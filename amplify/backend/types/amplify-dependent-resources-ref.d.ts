6352e6f8c9ee8dd9e85ddc0a5f2e10cff38484ca
Corey Lee Hurst <coreyhurst@hotmail.com>
2024-02-04 11:44:16 -0500

    Update amplify-dependent-resources-ref.d.ts and tsconfig.json

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,25 +1,5 @@
 export type AmplifyDependentResourcesAttributes = {
-  "function": {
-    "myportfoliosited3d913c7CustomMessage": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "myportfoliosited3d913c7PostConfirmation": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  },
   "storage": {
-    "PortfolioSiteAuth": {
-      "BucketName": "string",
-      "Region": "string"
-    },
     "myfeedback": {
       "Arn": "string",
       "Name": "string",
