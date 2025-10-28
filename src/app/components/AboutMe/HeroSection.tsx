9617a85a688a30a740c008fe2874853bc5dd2b0a
Corey Lee Hurst <coreyhurst@hotmail.com>
2025-10-28 07:06:27 -0400

    Update CV link in HeroSection and add new PDF document
    - Changed the CV link in the HeroSection component from the old PDF to the new one.
    - Added the new CV document for Corey Hurst as a PDF file in the images/documents directory.

--- src/app/components/AboutMe/HeroSection.tsx
+++ src/app/components/AboutMe/HeroSection.tsx
@@ -71,7 +71,7 @@ const HeroSection = () => {
 							Hire Me
 						</Link>
 						<Link
-							href="/cv/corey-hurst-cv.pdf"
+							href="/images/documents/10282025-Corey-Hurst-Senior Solutions Developer.pdf"
 							className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
 							target="_blank"
 							rel="noopener noreferrer"
