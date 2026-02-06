d46e74bd289513b448b7b006f5f70b7cb5326186
Corey Lee Hurst <coreyhurst@hotmail.com>
2026-02-06 03:23:32 -0500

    feat: Implement ProjectsSection component, project data, and a new project image. (#12)

--- src/app/components/Projects/ProjectsSection.tsx
+++ src/app/components/Projects/ProjectsSection.tsx
@@ -28,7 +28,7 @@ const ProjectsSection = () => {
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
-      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+      <div className="text-white flex flex-row flex-wrap justify-center items-center gap-2 py-6">
         <ProjectTag
           onClick={handleTagChange}
           name="All"
